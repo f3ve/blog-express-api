f08ced36c83fb074fbb8c4768850fe397a07bfc2
f3ve <free.eve@pm.me>
2021-01-27 13:14:53 -0800

    converted from promis.then to async/await

--- routes/articles.js
+++ routes/articles.js
@@ -12,38 +12,41 @@ const router = express.Router();
 
 router
   .route('/')
-  .get((req, res, next) => {
+  .get(async (req, res, next) => {
     /*
       Get a paginated list of public articles. can optionally filter by category
       or page. limit defaults to 20 but can be increased or decreased using 
       query params
     */
 
-    // optional values based on query params
-    limit = parseInt(req.query.limit) || 20;
-    page = (parseInt(req.query.page) - 1) * limit || 0;
-    category = parseInt(req.query.category);
+    try {
+      // optional values based on query params
+      limit = parseInt(req.query.limit) || 20;
+      page = (parseInt(req.query.page) - 1) * limit || 0;
+      category = parseInt(req.query.category);
 
-    Article.findAll({
-      where: {
-        draft: false,
-        // conditionally add CategoryId to where if category is not undefined
-        ...(category && { CategoryId: category }),
-      },
-      //paginate results
-      offset: page,
-      limit: limit,
-    })
-      .then((articles) =>
-        res.json({
-          results: articles,
-          total: articles.length,
-          page: page / 20 + 1 || 1,
-        })
-      )
-      .catch((err) => next(err));
+      const articles = await Article.findAll({
+        where: {
+          draft: false,
+          // conditionally add CategoryId to where if category is not undefined
+          ...(category && { CategoryId: category }),
+        },
+        //paginate results
+        offset: page,
+        limit: limit,
+      });
+
+      // format and return results
+      res.json({
+        results: articles,
+        total: articles.length,
+        page: page / 20 + 1 || 1,
+      });
+    } catch (err) {
+      next(err);
+    }
   })
-  .post(requireAuth, parser, (req, res, next) => {
+  .post(requireAuth, parser, async (req, res, next) => {
     /* 
       Creates a new article, requires authentication so currently only site 
       owner/admin can create articles.
@@ -57,25 +60,26 @@ router
       }
     */
 
-    Article.create({
-      title: req.body.title,
-      UserId: 1,
-      content: req.body.content,
-      publish_date: req.body.publish_date || new Date(),
-      slug: req.body.title
-        .toLowerCase()
-        .replace(/[^\w\s]|_/g, '')
-        .replace(/\s/g, '_'),
-      category: req.body.category,
-      draft: req.body.draft,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    })
-      .then((article) => {
-        res.status(202);
-        res.send(article);
-      })
-      .catch((err) => next(err));
+    try {
+      const article = await Article.create({
+        title: req.body.title,
+        UserId: 1,
+        content: req.body.content,
+        publish_date: req.body.publish_date || new Date(),
+        slug: req.body.title
+          .toLowerCase()
+          .replace(/[^\w\s]|_/g, '')
+          .replace(/\s/g, '_'),
+        category: req.body.category,
+        draft: req.body.draft,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+      res.status(202).send(article);
+    } catch (err) {
+      next(err);
+    }
   });
 
 router
@@ -88,51 +92,54 @@ router
 
     res.send(res.article);
   })
-  .delete(requireAuth, (req, res, next) => {
+  .delete(requireAuth, async (req, res, next) => {
     /* 
       deletes specified article. requires authentication.
     */
 
-    Article.destroy({
-      where: { slug: req.params.slug },
-    }).then(res.status(204).end());
+    try {
+      await Article.destroy({
+        where: { slug: req.params.slug },
+      });
+
+      res.status(204).end();
+    } catch (err) {
+      next(err);
+    }
   })
-  .patch(requireAuth, parser, (req, res, next) => {
+  .patch(requireAuth, parser, async (req, res, next) => {
     /* 
       update field values of specified article, requires authentication
     */
+    try {
+      const slug = req.body.title
+        .toLowerCase()
+        .replace(/[^\w\s]|_/g, '')
+        .replace(/\s/g, '_');
 
-    const slug = req.body.title
-      .toLowerCase()
-      .replace(/[^\w\s]|_/g, '')
-      .replace(/\s/g, '_');
-
-    Article.update(
-      {
-        title: req.body.title,
-        content: req.body.content,
-        updatedAt: new Date(),
-        slug,
-      },
-      {
-        where: {
-          slug: req.params.slug,
+      await Article.update(
+        {
+          title: req.body.title,
+          content: req.body.content,
+          updatedAt: new Date(),
+          slug,
         },
-      }
-    )
-      .then(() => {
-        Article.findOne({
+        {
           where: {
-            slug,
+            slug: req.params.slug,
           },
-        }).then((article) => {
-          console.log(article);
-          res.status(200).send(article);
-        });
-      })
-      .catch((err) => {
-        next(err);
+        }
+      );
+      const article = await Article.findOne({
+        where: {
+          slug,
+        },
       });
+
+      res.status.send(article);
+    } catch (err) {
+      next(err);
+    }
   });
 
 module.exports = router;
